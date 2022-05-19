866ae7478a79cd240108b5911ecfbef4a5691770
YoussefNour1 <yousnour222@gmail.com>
2022-05-19 17:29:42 +0200

    alpha update

--- product/urls.py
+++ product/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from product import views
 
 urlpatterns = [
-    path('', views.landing, name="landing")
+    path('', views.landing, name="landing"),
+    path('products/', views.all_products, name="all_products"),
 ]
--- product/views.py
+++ product/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def landing(request):
     return render(request, "product/landing.html")
+
+
+def all_products(request):
+    return render(request, "product/products.html")
